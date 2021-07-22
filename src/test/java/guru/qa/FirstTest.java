9294126caff10943baf903069f425946a7c2fc75
dtuchs_qaguru <d.tuchs@propellerads.net>
2021-07-22 21:27:18 +0300

    added logic

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -1,10 +1,13 @@
 package guru.qa;
 
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 public class FirstTest {
 
     @Test
     void firstTest() {
+        System.out.println("firstTest!");
+        Selenide.open("http://ya.ru");
     }
 }
