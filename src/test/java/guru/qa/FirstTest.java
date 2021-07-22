ed59e0fa10591a7ed6e7b55a63ee558908a3d940
dtuchs_qaguru <d.tuchs@propellerads.net>
2021-07-22 21:32:17 +0300

    changed text

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -7,7 +7,7 @@ public class FirstTest {
 
     @Test
     void firstTest() {
-        System.out.println("First Test!!!!");
+        System.out.println("firstTest!!!!");
         Selenide.open("http://ya.ru");
     }
 }
